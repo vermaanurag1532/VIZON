7531b5ad062f8c8c266d955139a0503825f2bbe4
vermaanurag1532 <vermaanurag1532@gmail.com>
2022-10-30 01:21:44 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import Header from "./components/Home/header";
+import Footer from "./components/Home/footer";
+import Navbar from "./components/Navbar/Navbar";
+import { Switch, Route } from "react-router-dom";
+import Cards from "./components/Cards/Cards";
+import "./App.css";
+const Home = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Header />
+      <Cards />
+      <Footer />
+    </>
   );
-}
+};
+
+const About = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="hero-section">
+        <p> Welcome to </p> <h1> Vizon Technical About Page </h1>{" "}
+      </section>{" "}
+    </>
+  );
+};
+
+const Service = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="hero-section">
+        <p> Welcome to </p> <h1> Vizon Technical Service Page </h1>{" "}
+      </section>{" "}
+    </>
+  );
+};
+
+const Contact = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="hero-section">
+        <p> Welcome to </p> <h1> Vizon Technical Contact Page </h1>{" "}
+      </section>{" "}
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/service">
+        <Service />
+      </Route>
+      <Route path="/contact">
+        <Contact />
+      </Route>{" "}
+    </Switch>
+  );
+};
 
 export default App;
